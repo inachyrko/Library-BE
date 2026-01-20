a200e084ed414a9dc227b5758114e1cbc920fd46
inach <inachyrko@gmail.co>
2026-01-20 12:10:37 +0100

    fix: add put method

--- src/main/java/org/example/mylibrarybe/service/BookController.java
+++ src/main/java/org/example/mylibrarybe/service/BookController.java
@@ -2,14 +2,7 @@ package org.example.mylibrarybe.service;
 
 import org.example.mylibrarybe.dao.BookRepository;
 import org.example.mylibrarybe.model.Book;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,6 +31,11 @@ public class BookController {
         return repository.save(book);
     }
 
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable String id, @RequestBody Book book) {
+        book.setId(id);
+        return repository.save(book);
+    }
     @DeleteMapping("/{id}")
     public void deleteBook(@PathVariable String id) {
         repository.deleteById(id);
